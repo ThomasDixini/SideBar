c722d536456de816c1ce16c4a0c29a42160d0df2
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-01-19 23:35:40 -0300

    Adicionando texto quando abre o menu agora, modificado: JS, css e HTML

--- script.js
+++ script.js
@@ -4,7 +4,9 @@
 const DOM = {
     menu: document.querySelector('.menu'),
     button: document.querySelector('button'),
-    icons: document.querySelectorAll('.icon')
+    icons: document.querySelectorAll('.icon'),
+    paragraphs: document.querySelectorAll('p')
+
 
 }
 
@@ -12,9 +14,33 @@ const DOM = {
 function openMenu() {
     DOM.menu.classList.toggle('aberto')
     DOM.button.classList.toggle('aberto')
-    DOM.icons.forEach(icon => icon.classList.toggle('aberto'))
+    DOM.icons.forEach(icon =>  {
+
+
+        icon.classList.toggle('aberto')
+
+        
+
+
+    })
+    
+    
+
+    DOM.paragraphs.forEach(paragraph => {
+
+        let hidden = "hidden"
+
+        if (paragraph.hasAttribute(hidden) == true){
+            paragraph.removeAttribute(hidden)
+        } else {
+            paragraph.setAttribute(hidden, true)
+        }
+
+        
+
+
+    })
+
+    
 }
 
-function selectIcon() {
-
-}
